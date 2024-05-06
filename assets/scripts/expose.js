4e41307a9d0d28345841a458e6b2b1ee40baf94c
p3reddy <p3reddy@ucsd.edu>
2024-05-05 21:20:58 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,41 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const horn_select = document.getElementById("horn-select");
+  const sound_image = document.querySelector("#expose > img");
+  const audio = document.querySelector("audio");
+
+  console.log(`assets/images/.png`)
+
+  horn_select.addEventListener("change", (event) => {
+    sound_image.src = `./assets/images/${event.target.value}.svg`;
+    audio.src = `./assets/audio/${event.target.value}.mp3`;
+  });
+
+
+  const volume = document.getElementById("volume");
+  const audio_image = document.querySelector("div > img");
+
+  volume.addEventListener("change", (event) => {
+    let level = 0;
+    if (event.target.value >= 67) {
+      level = 3;
+    } else if (event.target.value >= 33) {
+      level = 2;
+    } else if (event.target.value >= 1) {
+      level = 1;
+    }
+    audio_image.src = `./assets/icons/volume-level-${level}.svg`;
+    audio.volume = event.target.value / 100;
+  });
+
+  const play_button = document.querySelector("button");
+  play_button.addEventListener("click", () => {
+    if (horn_select.value === "party-horn") {
+      const jsConfetti = new JSConfetti()
+      jsConfetti.addConfetti()
+    }
+    audio.play();
+  })
+
 }
