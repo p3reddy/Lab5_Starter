7948d10b3c01253d441ece1e0e656b6b83bfcae2
p3reddy <p3reddy@ucsd.edu>
2024-05-06 00:31:24 -0700

    added unit tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Pranav Reddy
+
+1. No, because the "message" feature involves multiple components, such as the sending, receiving, displaying, etc., which cannot be easily encapsulated in unit tests.
+2. Yes, because this is a small feature with well-defined functionality that does not involve many outside components.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,89 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber
+
+test('phone number without area code', () => {
+  expect(isPhoneNumber("4567890")).toBe(false);
+});
+
+test('phone number with illegal characters', () => {
+  expect(isPhoneNumber("123#456#7890")).toBe(false);
+});
+
+test('phone number with international code', () => {
+  expect(isPhoneNumber("(1) 123-456-7890")).toBe(true);
+});
+
+test('phone number without dashes', () => {
+  expect(isPhoneNumber("(123)-456-7890")).toBe(true);
+});
+
+// isEmail
+test('email without @', () => {
+  expect(isEmail("p3reddyucsd.edu")).toBe(false);
+});
+
+test('email without url', () => {
+  expect(isEmail("p3reddy@")).toBe(false);
+});
+
+test('email with 2 character extension', () => {
+  expect(isEmail("p3reddy@ucsd.to")).toBe(true);
+});
+
+test('email with 3 character extension', () => {
+  expect(isEmail("p3reddy@ucsd.edu")).toBe(true);
+});
+
+// isStrongPassword
+test('too short password', () => {
+  expect(isStrongPassword("p3r")).toBe(false);
+});
+
+test('too long password', () => {
+  expect(isStrongPassword("Anb1234567890123")).toBe(false);
+});
+
+test('password with special characters', () => {
+  expect(isStrongPassword("AA12345AhB__")).toBe(true);
+});
+
+test('password without special characters', () => {
+  expect(isStrongPassword("as123456789")).toBe(true);
+});
+
+// isDate
+test('YYYY/MM/DD', () => {
+  expect(isDate("2024/05/05")).toBe(false);
+});
+
+test('MM/DD/YY', () => {
+  expect(isDate("05/05/24")).toBe(false);
+});
+
+test('M/D/YYYY', () => {
+  expect(isDate("5/5/2024")).toBe(true);
+});
+
+test('MM/DD/YYYY', () => {
+  expect(isDate("05/05/2024")).toBe(true);
+});
+
+// isHexColor
+test('Invalid Alphabetical Hex Digit', () => {
+  expect(isHexColor("#gcba03")).toBe(false);
+});
+
+test('7 character string', () => {
+  expect(isHexColor("#fcb00323")).toBe(false);
+});
+
+test('Lowercase hex string', () => {
+  expect(isHexColor("ffffff")).toBe(true);
+});
+
+test('Uppercase hex string', () => {
+  expect(isHexColor("FFFFFF")).toBe(true);
+});
