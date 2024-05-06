6ada00932436c9753349b8c823c6e23fe60d58a3
p3reddy <p3reddy@ucsd.edu>
2024-05-05 23:29:08 -0700

    finished explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,66 @@
 // explore.js
 
+
 window.addEventListener('DOMContentLoaded', init);
+let isSpeaking = false;
 
 function init() {
-  // TODO
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+
+  const voiceSelect = document.querySelector("#voice-select");
+  const text_area = document.querySelector("#text-to-speak");
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    const synth = window.speechSynthesis;
+    const voices = speechSynthesis.getVoices();
+
+
+    const utterThis = new SpeechSynthesisUtterance(text_area.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    utterThis.addEventListener("start", () => {
+      const face_image = document.querySelector("#explore > img");
+      face_image.src = "assets/images/smiling-open.png";
+    })
+    utterThis.addEventListener("end", () => {
+      const face_image = document.querySelector("#explore > img");
+      face_image.src = "assets/images/smiling.png";
+    })
+    synth.speak(utterThis);
+  })
+
+  isSpeaking.addEventListener
+}
+
+function populateVoiceList() {
+  if (typeof speechSynthesis === "undefined") {
+    return;
+  }
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
 }
